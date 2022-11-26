11d9152be26bfb936d9124a61da68c22ff4e65a7
Willy Eth <willyeth01@gmail.com>
2022-11-26 09:33:20 -0600

    metadata second commit

--- components/Metatags.js
+++ components/Metatags.js
@@ -2,8 +2,8 @@ import Head from 'next/head'
 
 export default function Metatags({
   title = 'My POC to Learn Next JS and Firebase',
-  description = 'A complete Next.js + Firebase course by Fireship.io',
-  image = 'https://firebasestorage.googleapis.com/v0/b/nextfire-1fc9e.appspot.com/o/uploads%2FUchdV35jMlRAFhtfwajpWSzAhxk1%2F1669475526358.png?alt=media&token=82acab2e-e1c9-44a7-82c4-6f9472942b31',
+  description = 'Personal Project to Learn Web3 Next.js + Firebase',
+   image = 'https://firebasestorage.googleapis.com/v0/b/nextfire-1fc9e.appspot.com/o/uploads%2FUchdV35jMlRAFhtfwajpWSzAhxk1%2F1669476656950.png?alt=media&token=2bea8ef3-9a76-4df8-85bb-25a7af282086',
 }) {
   return (
     <Head>
