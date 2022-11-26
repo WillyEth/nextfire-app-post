3a782e890422494190e1cbdfb0d831195f325ad1
Willy Eth <willyeth01@gmail.com>
2022-11-26 08:43:43 -0600

    init commit to repo

---
+++ components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Loader(props) {
+  return (
+
+     
+ props.show ? <div className="loader"></div> : null
+ 
+
+  )
+}
---
+++ pages/[username]/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import UserProfile from '../../components/UserProfile'
+import PostFeed from '../../components/PostFeed'
+import { getUserWithUsername, postToJSON } from '../../lib/firebase'
+
+export async function getServerSideProps({ query }) {
+  const { username } = query
+
+  const userDoc = await getUserWithUsername(username)
+
+  // JSON serializable data
+  let user = null
+  let posts = null
+
+  if (!userDoc) {
+    return {
+      notFound: true,
+    }
+  }
+
+  if (userDoc) {
+    user = userDoc.data()
+    const postsQuery = userDoc.ref
+      .collection('posts')
+      .where('published', '==', true)
+      .orderBy('createdAt', 'desc')
+      .limit(5)
+    posts = (await postsQuery.get()).docs.map(postToJSON)
+  }
+
+  return {
+    props: { user, posts }, // will be passed to the page component as props
+  }
+}
+
+export default function UserProfilePage({ user, posts }) {
+  return (
+    <div>
+      <UserProfile user={user} />
+      <PostFeed posts={posts} />
+    </div>
+  )
+}
